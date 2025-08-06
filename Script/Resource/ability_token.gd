ae47087bfe465b27a6aaa1fae2af5860a6558d4e
michsanl <desk.ichsan101@gmail.com>
2025-08-06 21:02:01 +0700

    refactor ability token

--- Script/Resource/ability_token.gd
+++ Script/Resource/ability_token.gd
@@ -3,11 +3,14 @@ extends Resource
 
 enum TokenType { NOTSET, ATTACK, GUARD, EVADE }
 
+@export var token_type: TokenType
 @export var min: int = 0
 @export var max: int = 0
-
-var token_type_test: TokenType
 
 
 func get_token_value() -> int:
 	return randi_range(min, max)
+
+
+func get_token_type() -> TokenType:
+	return token_type
--- Script/Resource/ability_token_guard.gd
+++
@@ -1,5 +0,0 @@
-@icon("res://Art/Guard_Icon.png")
-class_name AbilityTokenGuard
-extends AbilityToken
-
-@export var token_type:= TokenType.GUARD
