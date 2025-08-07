4ac6548533b4b53de9c1372e207f316b3f1a8dc7
michsanl <desk.ichsan101@gmail.com>
2025-08-08 00:10:43 +0700

    update character visual

--- Script/Character Component/Character Action/character_visual.gd
+++ Script/Character Component/Character Action/character_visual.gd
@@ -1,16 +1,8 @@
 class_name CharacterVisual
 extends Node
 
-enum VisualType { DEFAULT, SLASH, PIERCE, BLUNT, MOVE, BLOCK, DAMAGED }
-
 @export var sprite_node: Sprite2D
 @export var sprite_resource: CharacterSpriteResource
-@export var debug_visual: VisualType:
-	set(value):
-		_debug_visual = value
-		_on_debug_visual_changed(value)
-
-var _debug_visual: VisualType
 
 
 func change_to_default_sprite() -> void:
@@ -43,21 +35,3 @@ func change_to_damaged_sprite() -> void:
 
 func set_sprite(target_sprite: Texture2D) -> void:
 	sprite_node.texture = target_sprite
-
-
-func _on_debug_visual_changed(visual_type: VisualType):
-	match visual_type:
-		VisualType.DEFAULT:
-			change_to_default_sprite()
-		VisualType.SLASH:
-			change_to_slash_sprite()
-		VisualType.PIERCE:
-			change_to_pierce_sprite()
-		VisualType.BLUNT:
-			change_to_blunt_sprite()
-		VisualType.MOVE:
-			change_to_move_sprite()
-		VisualType.BLOCK:
-			change_to_block_sprite()
-		VisualType.DAMAGED:
-			change_to_damaged_sprite()
