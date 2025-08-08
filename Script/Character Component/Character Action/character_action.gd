c4724dd6e3eaf7c9e8f5fd2a724bdab6fe305f8b
michsanl <desk.ichsan101@gmail.com>
2025-08-08 23:05:16 +0700

    refactor combat state manager

--- Script/Character Component/Character Action/character_action.gd
+++ Script/Character Component/Character Action/character_action.gd
@@ -3,11 +3,11 @@ extends Node
 
 @export var character_movement: CharacterMovement
 @export var character_visual: CharacterVisual
+@export var attack_duration: float = 1.0
 
 func perform_move(this_character: CharacterBase, destination: Vector2):
 	character_visual.change_to_move_sprite()
 	await character_movement.move_position(this_character, destination)
-
 
 
 func perform_approach_target_one_sided(this_character: CharacterBase, target: CharacterBase):
@@ -26,6 +26,7 @@ func perform_approach_target_two_sided(this_character: CharacterBase, target: Ch
 
 func perform_slash_attack():
 	character_visual.change_to_slash_sprite()
+	await get_tree().create_timer(attack_duration).timeout
 	
 
 
