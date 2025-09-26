d358d513e922b4d88ec5a865c524467fb5d8b87c
michsanl <desk.ichsan101@gmail.com>
2025-09-26 16:20:29 +0700

    implement guard and evade dice

---
+++ Script/Character/Character Components/CharacterAbility/AbilityDice/guard_dice.gd
@@ -0,0 +1,26 @@
+class_name GuardDice
+extends IDice
+
+func _init() -> void:
+	dice_type = DiceType.GUARD
+
+func execute(source: CharacterController, opponent: CharacterController):
+	opponent.get_health().damage(roll_val)
+	var knockback_pos = get_knockback(source, opponent, knockback_power * 3)
+	opponent.apply_knockback(knockback_pos)
+	source.get_view().change_to_guard_sprite()
+	if vfx:
+		source.get_vfx().set_sprite(vfx)
+	await source.get_tree().create_timer(duration).timeout
+	source.get_view().change_to_default_sprite()
+	source.get_vfx().clear_vfx()
+
+func execute_draw(source: CharacterController, opponent: CharacterController):
+	var knockback_pos = get_knockback(source, opponent, knockback_power/2)
+	opponent.apply_draw_knockback(knockback_pos)
+	source.get_view().change_to_guard_sprite()
+	if vfx:
+		source.get_vfx().set_sprite(vfx)
+	await source.get_tree().create_timer(duration).timeout
+	source.get_view().change_to_default_sprite()
+	source.get_vfx().clear_vfx()
